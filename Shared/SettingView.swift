af16ab49ad0f05fdcc622a8eaabff278dd17ae83
alliixx <allison.nmn.xin@gmail.com>
2022-09-05 11:44:35 -0600

    latest version

--- Shared/SettingView.swift
+++ Shared/SettingView.swift
@@ -21,6 +21,7 @@ struct SettingView: View {
     @State var selectedHare = 2
     @State var selectedPerson = 3
     @State var selectedCpu = 4
+    @State var buttonColor = Color.gray
 
     var body: some View {
         VStack {
@@ -34,11 +35,14 @@ struct SettingView: View {
                     .padding(.trailing)
                 Spacer()
                 Picker(selection: $selectedCard, label: Text("Card")) {
-                    ForEach(0 ..< cardPicker.count) {
+                    ForEach(0 ..< cardPicker.count, id:\.self) {
                       Text(String(self.cardPicker[$0]))
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
+                .onChange(of: selectedCard, perform: { _ in
+                    buttonColor = Color.blue
+                })
             }
             .padding([.top, .leading, .trailing])
             
@@ -47,96 +51,25 @@ struct SettingView: View {
              .padding(.horizontal, 10)
              .background(Color.blue)
             
-            HStack {
-                Label("Difficulty", systemImage: "square.3.stack.3d")
-                    .padding([.top, .leading, .trailing])
-                Spacer()
-            }
-            VStack {
-                HStack {
-                    Image(systemName: "ladybug.fill").padding(.horizontal)
-                    Spacer()
-                    Picker(selection: $selectedBug,
-                           label: Text("remember \(self.diffPicker[selectedBug]) cards")) {
-                        ForEach(0 ..< diffPicker.count) {
-                          Text(String(self.diffPicker[$0]))
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    Spacer()
-                }.padding(.vertical)
-                
-                HStack {
-                    Image(systemName: "tortoise.fill").padding(.horizontal)
-                    Spacer()
-                    Picker(selection: $selectedTortoise,
-                           label: Text("remember \(self.diffPicker[selectedTortoise]) cards")) {
-                        ForEach(0 ..< diffPicker.count) {
-                            Text(String(self.diffPicker[$0]))
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    Spacer()
-                }.padding(.vertical)
-                
-                HStack {
-                    Image(systemName: "hare.fill").padding(.horizontal)
-                    Spacer()
-                    Picker(selection: $selectedHare,
-                           label: Text("remember \(self.diffPicker[selectedHare]) cards")) {
-                        ForEach(0 ..< diffPicker.count) {
-                            Text(String(self.diffPicker[$0]))
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    Spacer()
-                }.padding(.vertical)
-
-                HStack {
-                    Image(systemName: "person.crop.rectangle.fill").padding(.horizontal)
-                    Spacer()
-                    Picker(selection: $selectedPerson, label: Text("remember \(self.diffPicker[selectedPerson]) cards")) {
-                        ForEach(0 ..< diffPicker.count) {
-                            Text(String(self.diffPicker[$0]))
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    Spacer()
-                }.padding(.vertical)
-
-                HStack {
-                    Image(systemName: "desktopcomputer").padding(.horizontal)
-                    Spacer()
-                    Picker(selection: $selectedCpu,
-                           label: Text("remember \(self.diffPicker[selectedCpu]) cards")) {
-                        ForEach(0 ..< diffPicker.count) {
-                            Text(String(self.diffPicker[$0]))
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    Spacer()
-                }.padding(.vertical)
-            }
-            Spacer()
-            Divider()
-             .frame(height: 1)
-             .padding(.horizontal, 10)
-             .background(Color.blue)
+          
 
             Spacer()
 
             Button("Apply Settings") {
-                globalData.cardCount = cardPicker[selectedCard]
-                globalData.hSize = xPicker[selectedCard]
-                globalData.vSize = yPicker[selectedCard]
-                globalData.numBug = diffPicker[selectedBug]
-                globalData.numTortoise = diffPicker[selectedTortoise]
-                globalData.numHare = diffPicker[selectedHare]
-                globalData.numHuman = diffPicker[selectedPerson]
-                globalData.numCpu = diffPicker[selectedCpu]
+                if(buttonColor == Color.blue){
+                    globalData.cardCount = cardPicker[selectedCard]
+                    globalData.hSize = xPicker[selectedCard]
+                    globalData.vSize = yPicker[selectedCard]
+                    globalData.numBug = diffPicker[selectedBug]
+                    globalData.numTortoise = diffPicker[selectedTortoise]
+                    globalData.numHare = diffPicker[selectedHare]
+                    globalData.numHuman = diffPicker[selectedPerson]
+                    globalData.numCpu = diffPicker[selectedCpu]
+                    buttonColor = Color.gray
+                }
             }
             .padding()
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            .background(buttonColor)
             .foregroundColor(.white)
             .cornerRadius(10)
             Spacer()
